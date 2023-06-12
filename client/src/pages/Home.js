c528d718c33300065a4e8209f1a201a18276f717
handika <handikaalfalaq01@gmail.com>
2023-06-13 00:24:35 +0700

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from '../component/Home'
-import Container from 'react-bootstrap/container';
+import { Container }from 'react-bootstrap';
 
 function Index() {
     return (
